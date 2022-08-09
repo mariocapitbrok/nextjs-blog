b464944a063364ec187ae953602dd52dc99a7119
Mario Capitbrok <mario@capitbrok.com>
2022-08-09 12:17:48 -0500

    Polishing the Post Page.

--- .history/pages/posts/[id]_20220809120851.js
+++ .history/pages/posts/[id]_20220809120851.js
@@ -1,8 +1,6 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../utils/posts'
 import Head from 'next/head'
-import Date from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id)
@@ -24,16 +22,13 @@ export async function getStaticPaths() {
 const Post = ({ postData }) => {
   return (
     <Layout>
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
--- .history/pages/posts/[id]_20220809121510.js
+++ .history/pages/posts/[id]_20220809121510.js
@@ -2,7 +2,6 @@ import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../utils/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id)
@@ -27,13 +26,12 @@ const Post = ({ postData }) => {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
+      <br />
+      {postData.id}
+      <br />
+      <Date dateString={postData.date} />
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
